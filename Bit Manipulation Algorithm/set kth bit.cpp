b67da844dd33ffde2f75dc1df7cecd693c81f794
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-12-12 20:02:07 +0530

    Create set kth bit.cpp

---
+++ Bit Manipulation Algorithm/set kth bit.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int setKthBit(int n, int k)
+{
+    return ((1 << k) | n);
+}
+int main()
+{
+    int n = 10, k = 3;
+    printf("Kth bit set number = %d",setKthBit(n, k));
+    return 0;
+} 
