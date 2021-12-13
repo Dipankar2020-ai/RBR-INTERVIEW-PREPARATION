759f1e49ecfb5c44536e89f8f626e9d6591dc001
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-12-13 11:51:06 +0530

    Create Multiply 7 with the given number.cpp

---
+++ Bit Manipulation Algorithm/Multiply 7 with the given number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+   int ans=(n<<3)-n;
+   cout<<"Multiply 7 with the given number :"<<ans;
+
+    return 0;
+}
